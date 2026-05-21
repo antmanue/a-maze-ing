f49cd24cf8a2521cd2ce45b04fe15b099a4995d4
Pedro WSL <moromizatopedro@gmail.com>
2026-05-21 19:09:34 +0100

    Added mock print_maze function, and start integration between Cell and Maze

--- config.py
+++ config.py
@@ -13,30 +13,57 @@ def read_config(file_name: str) -> dict[str, str]:
             config[key] = value
         return config
 
+#   Given the bit position from right to left, calculates the bit
+#   value (0 or 1)
+def calculate_bit(value: int, bit_pos: int) -> int:
+    bit_weigth = 8
+    iterations = [4, 3, 2, 1]
+    for _ in range(iterations[bit_pos]):
+        bit = int(value / bit_weigth)
+        value = value % bit_weigth
+        bit_weigth /= 2
+    return bit
+
 
 class Cell:
-    def __init__(self, west: int, south:int, east:int, north: int) -> None:
-        self.west = west
-        self.south = south
-        self.east = east
-        self.north = north
-        self.bits = west * 8 + south * 4 + east * 2 + north
+    def __init__(self, value: int, x: int, y: int) -> None:
+        self.value = value
+        self.west = calculate_bit(value, 3)
+        self.south = calculate_bit(value, 2)
+        self.east = calculate_bit(value, 1)
+        self.north = calculate_bit(value, 0)
+        self.x = x
+        self.y = y
 
-    def set_walls(self, bits: int):
-        self.bits = bits
-        self.west = int(bits / 8)
-        self.south = int((bits % 8) / 4)
-        self.east = int(((bits % 8) % 4) / 2)
-        self.north = int((((bits % 8) % 4) % 2) / 1)
+    def update_value(self, value: int):
+        self.value = value    # 4 bits (WSEN) from 0 - 15 => (0000) - (1111)
+        self.west = calculate_bit(value, 3)
+        self.south = calculate_bit(value, 2)
+        self.east = calculate_bit(value, 1)
+        self.north = calculate_bit(value, 0)
 
-    def get_bits(self):
-        print(f"cell: {self.west}{self.south}{self.east}{self.north} ", end='')
+    def set_bit(self, bit_pos: int, bit_value: int):
+        bit_weigth: int = pow(2, bit_pos)
+        if bit_value == 1:
+            if calculate_bit(self.value, bit_pos) != 1:
+                self.update_value(self.value + bit_weigth)
+        else:
+            if calculate_bit(self.value, bit_pos) != 0:
+                self.update_value(self.value - bit_weigth)
+
+    def get_bits(self) -> None:
+        [print(calculate_bit(self.value, x), end='')
+         for x in reversed(range(4))]
+        # print()
+
+    def print(self):
+        # print(f"cell: {self.get_bits()}")
         self.print_if(self.north, "_")
-        print("\n          ", end='')
+        # print("\n          ", end='')
         self.print_if(self.west, '|')
         self.print_if(self.south, '_')
         self.print_if(self.east, "|\n")
-    
+
     def print_if(self, exist: int, char: str):
         if exist:
             print(f"{char}", end='')
--- maze.py
+++ maze.py
@@ -2,6 +2,7 @@
 
 
 import config as conf
+import random as rand
 
 
 class MazeGenerator:
@@ -14,6 +15,7 @@ class MazeGenerator:
                            config["EXIT"].split(',')])
         self.output_file = config["OUTPUT_FILE"]
         self.perfect = bool(config["PERFECT"])
+        self.rows: list[list[conf.Cell]] = []
 
     def show(self) -> None:
         print(f"WIDTH: {self.width} ({type(self.width)})")
@@ -27,14 +29,60 @@ class MazeGenerator:
         print(f"\nOUTPUT_FILE: {self.output_file} ({type(self.output_file)})")
         print(f"PERFECT: {self.perfect} ({type(self.perfect)})")
 
+    def print_maze(self) -> None:
+        for row in self.rows:
+            if row[0].y == 0:   # first_row
+                print(' ', end='')
+                for cell in row:
+                    if cell.y == 0 and cell.north:
+                        print('_ ', end='')
+                print()
+            for cell in row:
+                cell.print_if(cell.west, '|')
+                cell.print_if(cell.south, '_')
+                if cell.x == self.width - 1:
+                    cell.print_if(cell.east, '|')
+                    print()
 
 def main() -> None:
     config = conf.read_config("config.txt")
     maze = MazeGenerator(config)
     maze.show()
+    print()
 
-    cell = conf.Cell(1,1,1,1)
-    cell.get_bits()
+    rows: list[list[conf.Cell]] = []
+    for y in range(maze.height):
+        columns: list[conf.Cell]= []
+        for x in range(maze.width):
+            west = 3
+            south = 2
+            east = 1
+            north = 0
+            value = rand.randint(0, 15)
+            cell = conf.Cell(value, x, y)
+            # print(f"value: {cell.value} - ", end='')
+            # cell.get_bits()
+            if y == 0:
+                cell.set_bit(north, 1)
+            if y == maze.height - 1:
+                cell.set_bit(south, 1)
+            if x == 0:
+                cell.set_bit(west, 1)
+            if x == maze.width - 1:
+                cell.set_bit(east, 1)
+            # print(f"        value: {cell.value} - ", end='')
+            # cell.get_bits()
+            # print()
+            # columns.append(cell)
+            columns.append(cell)
+            print(hex(cell.value).split('0x')[1].upper(), end='')
+        print()
+        # rows.append(columns)
+        maze.rows.append(columns)
+    # maze.rows[0][0].get_bits()
+    maze.print_maze()
+
+
 
 if __name__ == "__main__":
     main()
