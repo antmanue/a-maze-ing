0a84d5fb832d7f689afab5e41da3ca5298a6919b
antmanue <antmanue@student.42porto.com>
2026-05-21 18:54:55 +0100

    feat: open window, X button to close, ESC button to close

--- mazegen/display.py
+++ mazegen/display.py
@@ -1,5 +1,6 @@
 from typing import List, Any
 from mlx import Mlx
+import os
 
 class MazeDisplay:
     def __init__(self) -> None:
@@ -8,10 +9,13 @@ class MazeDisplay:
             [12, 0, 2],
             [5, 4, 6]
         ]
+        self.block_size: int = 64
         self.m: Mlx = Mlx()
         self.mlx_ptr: Any = self.m.mlx_init()
         self.win_ptr: Any = self.m.mlx_new_window(self.mlx_ptr, 800, 600, "A-Maze-ing")
-    
+        self.m.mlx_hook(self.win_ptr, 33, 0, self.close_window, 0)
+        self.m.mlx_hook(self.win_ptr, 2, 1, self.handle_keypress, 0)
+
     def render_terminal(self) -> None:
         print("=== Test Maze ===")
         for row in self.grid:
@@ -19,5 +23,28 @@ class MazeDisplay:
             print(formated_line)
 
     def run(self) -> None:
-        print("A abrir a janela gráfica...")
+        print("Opening A-Maze-ing graphic window...")
+        self.draw_maze()
         self.m.mlx_loop(self.mlx_ptr)
+
+    def close_window(self, *args: Any) -> int:
+            print("Closing A-Maze-ing...")
+            #self.m.mlx_destroy_window(self.mlx_ptr, self.win_ptr)
+            os._exit(0)
+
+    def handle_keypress(self, keycode: int, param: int) -> int:
+        print(f"Pressed key: {keycode}")
+        if keycode == 65307:
+            os._exit(0)
+        return 0
+
+    def draw_maze(self) -> None:
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[row])):
+                x = col * self.block_size
+                y = row * self.block_size
+
+                cell_value = self.grid[row][col]
+                self.draw_block(x, y, 0xFFFFFF)
+
+        
