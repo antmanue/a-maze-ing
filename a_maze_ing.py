a0700b0541d4b75db86379b7a6e0a6c3d510fe82
Pedro Henrique Panno Moromizato <pedrohe3@c2r2s9.42porto.com>
2026-05-22 19:36:09 +0100

    Separated MazeGenerator and Cell classes, and divided some functions into smaller and more concise ones. Still more refactor needed

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -16,63 +16,11 @@ class MazeGenerator:
                            config["EXIT"].split(',')])
         self.output_file = config["OUTPUT_FILE"]
         self.perfect = bool(config["PERFECT"])
-        self.rows: list[list[MazeGenerator.Cell]] = []
-        self.output = self.generate_map()
-        self.map = self.draw_map()
-        self.generate_output()
+        self.rows: list[list[conf.Cell]] = []
+        self.generate_map()
 
     def __str__(self) -> str:
-        return '\n'.join([self.output, self.map])
-
-    
-    class Cell:
-        def __init__(self, value: int, x: int, y: int) -> None:
-            self.value = value
-            self.west = self.calculate_bit(value, 3)
-            self.south = self.calculate_bit(value, 2)
-            self.east = self.calculate_bit(value, 1)
-            self.north = self.calculate_bit(value, 0)
-            self.x = x
-            self.y = y
-
-        @staticmethod
-        #   Given the bit position from right to left, calculates the bit
-        #   value (0 or 1)
-        def calculate_bit(value: int, bit_pos: int) -> int:
-            bit_weigth = 8
-            iterations = [4, 3, 2, 1]
-            bit = 0
-            for _ in range(iterations[bit_pos]):
-                bit = int(value / bit_weigth)
-                value = value % bit_weigth
-                bit_weigth = int(bit_weigth / 2)
-            return bit
-
-        def update_value(self, value: int):
-            self.value = value    # 4 bits (WSEN) from 0 - 15 => (0000) - (1111)
-            self.west = self.calculate_bit(value, 3)
-            self.south = self.calculate_bit(value, 2)
-            self.east = self.calculate_bit(value, 1)
-            self.north = self.calculate_bit(value, 0)
-
-        def set_bit(self, bit_pos: int, bit_value: int):
-            bit_weigth: int = pow(2, bit_pos)
-            if bit_value == 1:
-                if self.calculate_bit(self.value, bit_pos) != 1:
-                    self.update_value(self.value + bit_weigth)
-            else:
-                if self.calculate_bit(self.value, bit_pos) != 0:
-                    self.update_value(self.value - bit_weigth)
-
-        def get_bits(self) -> None:
-            [print(self.calculate_bit(self.value, x), end='')
-            for x in reversed(range(4))]
-
-        def draw_if(self, exist: int, char: str):
-            if exist:
-                return f"{char}"
-            else:
-                return " "
+        return self.draw_map()
 
     def show_config(self) -> None:
         print(f"WIDTH: {self.width} ({type(self.width)})")
@@ -85,6 +33,88 @@ class MazeGenerator:
          in self.exit]
         print(f"\nOUTPUT_FILE: {self.output_file} ({type(self.output_file)})")
         print(f"PERFECT: {self.perfect} ({type(self.perfect)})")
+
+    def generate_map(self) -> None:
+        for y in range(self.height):
+            columns: list[conf.Cell] = []
+            self.rows.append(columns)
+            for x in range(self.width):
+                value = rand.randint(0, 15)
+                cell = conf.Cell(value, x, y)
+                columns.append(cell)
+        self.validate_map()
+
+    def validate_map(self) -> None:
+        map_hex: list[str] = []
+        for row in self.rows:
+            row_output = ""
+            for cell in row:
+                self.generate_borders(cell)
+                self.validate_walls(cell)
+                row_output += hex(cell.value).split('0x')[1].upper()
+            map_hex.append(row_output)
+        self.generate_output(map_hex)
+
+    def generate_borders(self, cell: conf.Cell) -> None:
+        west = 3
+        south = 2
+        east = 1
+        north = 0
+        if cell.y == 0:
+            cell.set_bit(north, 1)
+        if cell.y == self.height - 1:
+            cell.set_bit(south, 1)
+        if cell.x == 0:
+            cell.set_bit(west, 1)
+        if cell.x == self.width - 1:
+            cell.set_bit(east, 1)
+
+    def validate_walls(self, cell: conf.Cell) -> None:
+        west = 3
+        north = 0
+        up = cell.y - 1
+        left = cell.x - 1
+        if up >= 0:
+            upper_cell = self.rows[up][cell.x]
+            self.check_wall(upper_cell, north, cell)
+        if left >= 0:
+            left_cell = self.rows[cell.y][left]
+            self.check_wall(left_cell, west, cell)
+
+    def check_wall(self, neighboor: conf.Cell, where: int,
+                   cell: conf.Cell) -> None:
+        west = 3
+        south = 2
+        east = 1
+        north = 0
+        orientation = -1
+        if where == north:
+            orientation = south
+        elif where == west:
+            orientation = east
+        calculate_bit = conf.Cell.calculate_bit
+        neighboor_wall = calculate_bit(neighboor.value, orientation)
+        cell_wall = calculate_bit(cell.value, where)
+        if neighboor_wall != cell_wall:
+            cell.set_bit(where, neighboor_wall)
+
+    def generate_output(self, map_hex: list[str]) -> None:
+        output = ""
+        output += '\n'.join(map_hex) + "\n\n"
+        output += str(self.entry) + '\n'
+        output += str(self.exit)
+        # output += self.findpath()
+        with open(self.output_file, 'w') as file:
+            file.write('\n'.join(map_hex) + '\n\n')
+            file.write(str(self.entry) + '\n')
+            file.write(str(self.exit))
+
+    def erase_map(self) -> None:
+        self.rows.clear()
+
+    def regenerate_map(self) -> None:
+        self.erase_map()
+        self.generate_map()
 
     def draw_map(self) -> str:
         map: list[str] = []
@@ -106,7 +136,8 @@ class MazeGenerator:
                 row_repr += cell.draw_if(cell.west, '|')
                 if coord == self.entry or coord == self.exit:
                     if cell.south:
-                        row_repr += cell.draw_if(cell.south, "\033[4m" + fill + "\033[0m")
+                        row_repr += cell.draw_if(cell.south, "\033[4m" +
+                                                 fill + "\033[0m")
                     else:
                         row_repr += fill
                 else:
@@ -115,80 +146,6 @@ class MazeGenerator:
                     row_repr += cell.draw_if(cell.east, '|')
             map.append(row_repr)
         return '\n'.join(map)
-
-    def generate_output(self) -> None:
-        with open(self.output_file, 'w') as file:
-            file.write(self.output + '\n\n')
-            file.write(str(self.entry) + '\n')
-            file.write(str(self.exit))
-
-    def generate_map(self) -> str:
-        output: list[str] = []
-        for y in range(self.height):
-            columns: list[MazeGenerator.Cell]= []
-            row_output = ""
-            self.rows.append(columns)
-            for x in range(self.width):
-                value = rand.randint(0, 15)
-                cell = MazeGenerator.Cell(value, x, y)
-                self.generate_borders(cell)
-                # columns.append(cell)
-                # print("Prev - ", end='')
-                # [print(hex(x.value).split('0x')[1].upper(), end='') for x in columns]
-                # print()
-                # print(self.draw_map())
-                self.validate_walls(cell)
-                # columns.pop()
-                columns.append(cell)
-                # print("Later - ", end='')
-                # [print(hex(x.value).split('0x')[1].upper(), end='') for x in columns]
-                # print()
-                # print(self.draw_map())
-                # print("--------------")
-                row_output += hex(cell.value).split('0x')[1].upper()
-            output.append(row_output)
-        return '\n'.join(output)
-
-    def generate_borders(self, cell: Cell) -> None:
-        west = 3
-        south = 2
-        east = 1
-        north = 0
-        if cell.y == 0:
-            cell.set_bit(north, 1)
-        if cell.y == self.height - 1:
-            cell.set_bit(south, 1)
-        if cell.x == 0:
-            cell.set_bit(west, 1)
-        if cell.x == self.width - 1:
-            cell.set_bit(east, 1)
-
-    def validate_walls(self, cell: Cell) -> None:
-        west = 3
-        north = 0
-        up = cell.y - 1
-        left = cell.x - 1
-        if up >= 0:
-            upper_cell = self.rows[up][cell.x]
-            self.check_wall(upper_cell, north, cell)
-        if left >= 0:
-            left_cell = self.rows[cell.y][left]
-            self.check_wall(left_cell, west, cell)
-
-    def check_wall(self, neighboor: Cell, where: int, cell: Cell) -> None:
-        west = 3
-        south = 2
-        east = 1
-        north = 0
-        orientation = -1
-        if where == north:
-            orientation = south
-        elif where == west:
-            orientation = east
-        neighboor_wall = MazeGenerator.Cell.calculate_bit(neighboor.value, orientation)
-        cell_wall = MazeGenerator.Cell.calculate_bit(cell.value, where)
-        if neighboor_wall != cell_wall:
-            cell.set_bit(where, neighboor_wall)
 
 
 def main() -> None:
@@ -201,6 +158,10 @@ def main() -> None:
     maze.show_config()
     print()
 
+    print(maze)
+    print()
+
+    maze.regenerate_map()
     print(maze)
     print()
 
--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 
-import typing
-
-
 def read_config(file_name: str) -> dict[str, str]:
     with open(file_name) as file:
         config: dict[str, str] = {}
@@ -12,3 +9,53 @@ def read_config(file_name: str) -> dict[str, str]:
             key, value = line.split('=')
             config[key] = value
         return config
+
+
+class Cell:
+    def __init__(self, value: int, x: int, y: int) -> None:
+        self.value = value
+        self.west = self.calculate_bit(value, 3)
+        self.south = self.calculate_bit(value, 2)
+        self.east = self.calculate_bit(value, 1)
+        self.north = self.calculate_bit(value, 0)
+        self.x = x
+        self.y = y
+
+    @staticmethod
+    #   Given the bit position from right to left, calculates the bit
+    #   value (0 or 1)
+    def calculate_bit(value: int, bit_pos: int) -> int:
+        bit_weigth = 8
+        iterations = [4, 3, 2, 1]
+        bit = 0
+        for _ in range(iterations[bit_pos]):
+            bit = int(value / bit_weigth)
+            value = value % bit_weigth
+            bit_weigth = int(bit_weigth / 2)
+        return bit
+
+    def update_value(self, value: int) -> None:
+        self.value = value    # 4 bits (WSEN) from 0-15 => (0000)-(1111)
+        self.west = self.calculate_bit(value, 3)
+        self.south = self.calculate_bit(value, 2)
+        self.east = self.calculate_bit(value, 1)
+        self.north = self.calculate_bit(value, 0)
+
+    def set_bit(self, bit_pos: int, bit_value: int) -> None:
+        bit_weigth: int = pow(2, bit_pos)
+        if bit_value == 1:
+            if self.calculate_bit(self.value, bit_pos) != 1:
+                self.update_value(self.value + bit_weigth)
+        else:
+            if self.calculate_bit(self.value, bit_pos) != 0:
+                self.update_value(self.value - bit_weigth)
+
+    def get_bits(self) -> None:
+        [print(self.calculate_bit(self.value, x), end='')
+            for x in reversed(range(4))]
+
+    def draw_if(self, exist: int, char: str) -> str:
+        if exist:
+            return f"{char}"
+        else:
+            return " "
